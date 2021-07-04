f9d3dc95aca4f054ed2882bab38126b7e24c8590
jariwalaprayag <jariwalaprayag98@gmail.com>
2021-07-03 22:12:24 -0500

    first commit

--- main.py
+++ main.py
@@ -1,62 +1,76 @@
 import sys
 
 def get_input_courses(courses_file):
-	courses = []
+	courses = {}
 	flag = 0
 	for course in courses_file:
 		if flag == 0:
 			flag = 1
 		else:
-			course_dict = {}
 			course_info = course.split(",")
-			course_dict["id"] = int(course_info[0])
-			course_dict["name"] = course_info[1]
-			course_dict["teacher"] = course_info[2][:-1]
-			courses.append(course_dict)
+			course_id = int(course_info[0])
+			courses[course_id] = {}
+			courses[course_id]["name"] = course_info[1]
+			courses[course_id]["teacher"] = course_info[2][:-1]
 	return courses
 
 def get_input_students(students_file):
-	students = []
+	students = {}
 	flag = 0
 	for student in students_file:
 		if flag == 0:
 			flag = 1
 		else:
-			student_dict = {}
 			student_info = student.split(",")
-			student_dict["id"] = int(student_info[0])
-			student_dict["name"] = student_info[1][:-1]
-			students.append(student_dict)
+			student_id = int(student_info[0])
+			students[student_id] = {}
+			students[student_id]["name"] = student_info[1][:-1]
 	return students
 
 def get_input_tests(tests_file):
-	tests = []
+	tests = {}
 	flag = 0
 	for test in tests_file:
 		if flag == 0:
 			flag = 1
 		else:
-			test_dict = {}
 			test_info = test.split(",")
-			test_dict["id"] = int(test_info[0])
-			test_dict["course_id"] = int(test_info[1])
-			test_dict["weight"] = int(test_info[2][:-1])
-			tests.append(test_dict)
+			test_id = int(test_info[0])
+			tests[test_id] = {}
+			tests[test_id]["course_id"] = int(test_info[1])
+			tests[test_id]["weight"] = int(test_info[2][:-1])
 	return tests
 
 def get_input_marks(marks_file):
-	marks = []
+	marks = {}
 	flag = 0
 	for mark in marks_file:
 		if flag == 0:
 			flag = 1
 		else:
-			mark_dict = {}
 			mark_info = mark.split(",")
-			mark_dict["test_student_id"] = [int(mark_info[0]) , int(mark_info[1])]
-			mark_dict["mark"] = int(mark_info[2][:-1])
-			marks.append(mark_dict)
+			student_id = int(mark_info[1])
+			if(student_id in marks.keys()):
+				marks[student_id].append([int(mark_info[0]), int(mark_info[2][:-1])])
+			else:
+				marks[student_id] = [[int(mark_info[0]), int(mark_info[2][:-1])]]
 	return marks
+
+def check_test_weights(tests):
+	valid_tests = {}
+	for test in tests.values():
+		if(test["course_id"] in valid_tests.keys()):
+			valid_tests[test["course_id"]]["weight"] += test["weight"]
+			valid_tests[test["course_id"]]["count"] += 1
+		else:
+			valid_tests[test["course_id"]] = {}
+			valid_tests[test["course_id"]]["weight"] = test["weight"]
+			valid_tests[test["course_id"]]["count"] = 1
+	return valid_tests
+
+def generate_output(courses, students, test, marks):
+	 valid_tests = check_test_weights(test)
+	 return valid_tests
 
 
 if(len(sys.argv) != 6):
@@ -71,20 +85,18 @@ path_to_output_file = sys.argv[5]
 
 courses_file = open(path_to_courses_file, "r")
 courses = get_input_courses(courses_file)
-print(courses)
 
 students_file = open(path_to_students_file, "r")
 students = get_input_students(students_file)
-print(students)
 
 tests_file = open(path_to_tests_file, "r")
 tests = get_input_tests(tests_file)
-print(tests)
 
 marks_file = open(path_to_marks_file, "r")
 marks = get_input_marks(marks_file)
-print(marks)
 
-# output_file = open(path_to_output_file, "w")
+output_file = open(path_to_output_file, "w")
+output = generate_output(courses, students, tests, marks)
+print(output)
 
 
